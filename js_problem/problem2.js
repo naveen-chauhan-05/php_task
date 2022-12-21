cb127dfd4923f98d6213d3fe536ab990792a3ec0
Codedrill Infotech Pvt Ltd <kanchan.codedrill@gmail.com>
2022-12-21 18:26:39 +0530

    add js

--- js_problem/problem2.js
+++ js_problem/problem2.js
@@ -1,9 +1,9 @@
 const fruitDb = ["apple", "bnana", "orange", "pineapple", "grapes", "avacodo", "strawberry"];
 
 function filterFruit(fruiDb){
-console.log(fruitDb);
+ 
 const mixFruit = ["grapes", "cabbage", "tomato", "bnana"];
-console.log(mixFruit);
+ 
 const onlyFruit =[];
 for (let index = 0; index < fruiDb.length; index++) {
   for (let index1 = 0; index1 < mixFruit.length; index1++) {
@@ -14,4 +14,10 @@ for (let index = 0; index < fruiDb.length; index++) {
 }
 return onlyFruit;
 }
-document.getElementById('second').innerHTML = filterFruit(fruitDb);
+ const answer = filterFruit(fruitDb);
+  let ans = "";
+for (let index = 0; index < answer.length; index++) {
+  
+  ans += answer[index]+" ";
+}
+document.getElementById('second').innerHTML = ans;
