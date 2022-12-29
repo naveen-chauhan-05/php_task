3dcc26d1622b2bc69a08fe162bb107ff86ff0071
Codedrill Infotech Pvt Ltd <kanchan.codedrill@gmail.com>
2022-12-29 11:49:23 +0530

    add advanc problem 1

--- php_advance/all_log.php
+++ php_advance/all_log.php
@@ -3,23 +3,45 @@ namespace Container;
 class Log{
     public static $fileName;
 
-    public static function addWarn($logMsg, $fname = null){
+    public function addWarn($logMsg, $fname = null){
         if($fname!=Null){
             $fname = $fname;
         }
         else{
             $fname = self::$fileName;
         }
-        self::addData($logMsg, $fame, "| DATA |");
+        self::addData($logMsg, $fname, "| Warning |");
+    }
+    public function addErr($logMsg, $fname=NULL){
+        if($fname!=NULL){
+            $fname = $fname;
+        }
+        else{
+            $fname = self::$fileName;
+        }
+        self::addData($logMsg, $fname, "| ERROR |");
+    }
+    public function addNotice($logMsg, $fname=NULL){
+        if($fname!=NULL){
+            $fname = $fname;
+        }
+        else{
+            $fname = self::$fileName;
+        }
+        self::addData($logMsg, $fname, "| Notice |"); 
     }
 
-    private static function addData($logMsg,$fname, $type)
+    private static function addData($logMsg, $fname, $type)
     {
-        $fopen  = fopen($fname, 'a+');
-        fwrite($fopen, " {$logMsg} sdafsdf: {$type}");  
-         echo "hello";
+        $fopen  = fopen($fname, "a+");
+        $value = "\n\n";
+        $value.=fwrite($fopen, "\n{$type}======> {$logMsg} ");  
+         
     }
 }
-
-Log::addWarn("hello this is my message", "php_advance/store_log.txt");
+Log::$fileName = "text.txt";
+ $obj = new Log();
+$obj->addErr("This is Error");
+$obj->addWarn("Hello This is Warning", "text.txt");
+$obj->addNotice("This is Notice");
 ?>
